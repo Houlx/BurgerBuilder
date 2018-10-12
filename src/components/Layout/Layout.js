ad27d233297f31e0cae90a99cb62763e0ce86284
Hou <houlxssdut@gmail.com>
2018-10-12 23:48:47 +0800

    basic version

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,22 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-  <>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </>
-)
+class Layout extends Component {
+  state = {
+    showSideDrawer: false,
+  }
 
-layout.propTypes = {
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false })
+  }
+
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => ({ showSideDrawer: !prevState.showSideDrawer }))
+  }
+
+  render() {
+    return (
+      <>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler} />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </>
+    )
+  }
+}
+
+Layout.propTypes = {
   children: PropTypes.object,
 }
 
-export default layout
+export default Layout
--- src/components/UI/Model/Model.js
+++ src/components/UI/Model/Model.js
@@ -1,29 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import classes from './Model.module.css'
 import Backdrop from '../Backdrop/Backdrop'
 
-const model = (props) =>
-  <>
-    <Backdrop
-      show={props.show}
-      clicked={props.modelClosed} />
-    <div
-      className={classes.Model}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0',
-      }}
-    >
-      {props.children}
-    </div>
-  </>
+class Model extends Component {
 
-model.propTypes = {
+  /* eslint no-unused-vars: 0 */
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show
+  }
+
+  render() {
+    return (
+      <>
+        <Backdrop
+          show={this.props.show}
+          clicked={this.props.modelClosed} />
+        <div
+          className={classes.Model}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0',
+          }}
+        >
+          {this.props.children}
+        </div>
+      </>
+    )
+  }
+}
+
+
+Model.propTypes = {
   children: PropTypes.object,
   show: PropTypes.bool,
   modelClosed: PropTypes.func,
 }
 
-export default model
+export default Model
